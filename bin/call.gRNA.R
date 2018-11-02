3287c7e0754a45a08d75115e0e3eddd54023bde7
Michael MW <wangmengchi@gmail.com>
2018-11-01 19:21:22 -0700

    pdf->png QC plot for faster rendering

--- bin/call.gRNA.R
+++ bin/call.gRNA.R
@@ -180,8 +180,16 @@ p2 <- ggplot(tab, aes(x=logFC,y=PValue)) +
   geom_point(aes(colour = status), size = 1) + theme_classic()
 
 ## output all figures to file
-outPdf = paste0(outDir, "/", paste0(prefix, ".qc.pdf"))
-cat(paste0("plot QC file = ", outPdf,"\n"))
-pdf(outPdf, width = 12, height = 10)
+## pdf (high quality but slow rendering)
+# outPdf = paste0(outDir, "/", paste0(prefix, ".qc.pdf"))
+# cat(paste0("plot QC file = ", outPdf,"\n"))
+# pdf(outPdf, width = 12, height = 10)
+# grid.arrange(ppca1, ppca2, p1, p2, nrow = 2)
+# dev.off()
+
+## png
+outPng = paste0(outDir, "/", paste0(prefix, ".qc.png"))
+cat(paste0("plot QC file = ", outPng,"\n"))
+png(outPng, width = 900, height = 700)
 grid.arrange(ppca1, ppca2, p1, p2, nrow = 2)
 dev.off()
